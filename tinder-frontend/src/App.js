7d4479047ef5223336e43829b44356b8b775108a
Kingswhale <hardeyreemyarfyc@gmail.com>
2022-03-20 18:33:20 +0100

    header update

--- tinder-frontend/src/App.js
+++ tinder-frontend/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react'
 import './App.css'
+import Header from './Header'
 
 function App() {
   //BEM class naming convention
-  return <div className='App'> MERN HEADER</div>
+  return (
+    <div className='app'>
+      <h1>MEANFHFHHF</h1>
+      <Header />
+      {/* Header */}
+      {/* tinder card */}
+      {/* swip button */}
+    </div>
+  )
 }
 
 export default App

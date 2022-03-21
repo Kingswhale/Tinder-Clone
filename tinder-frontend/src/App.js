9b2186cbccf4c2f377f8bcd7fafa8a6f7870b658
Kingswhale <hardeyreemyarfyc@gmail.com>
2022-03-21 23:24:56 +0100

    wip

--- tinder-frontend/src/App.js
+++ tinder-frontend/src/App.js
@@ -6,9 +6,7 @@ function App() {
   //BEM class naming convention
   return (
     <div className='app'>
-      <h1>MEANFHFHHF</h1>
       <Header />
-      {/* Header */}
       {/* tinder card */}
       {/* swip button */}
     </div>
--- tinder-frontend/src/Header.js
+++ tinder-frontend/src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
-
+import PersonIcon from '@material-ui/icons/Person'
 function Header() {
   return (
     <div className='header'>
       <h2>I am the Header</h2>
+      <PersonIcon className='header_icon' fontSize='large' />
     </div>
   )
 }

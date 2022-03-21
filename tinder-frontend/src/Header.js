46e6960bf479bcc14584d81de763f793acee29c9
Kingswhale <hardeyreemyarfyc@gmail.com>
2022-03-22 00:40:32 +0100

    header

--- tinder-frontend/src/Header.js
+++ tinder-frontend/src/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
 import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
 
 function Header() {
   return (
@@ -14,6 +15,9 @@ function Header() {
         src='https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg'
         alt='tinde logo'
       />
+      <IconButton>
+        <ForumIcon className='header_icon' fontSize='large' />
+      </IconButton>
     </div>
   )
 }

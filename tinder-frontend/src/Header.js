e3792fd630c109905e85d45017f24ac6c4833c62
Kingswhale <hardeyreemyarfyc@gmail.com>
2022-03-21 23:54:31 +0100

    header

--- tinder-frontend/src/Header.js
+++ tinder-frontend/src/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+
 function Header() {
   return (
     <div className='header'>
       <h2>Header</h2>
-      <PersonIcon className='header_icon' fontSize='large' />
+      <IconButton>
+        <PersonIcon className='header_icon' fontSize='large' />
+      </IconButton>
     </div>
   )
 }

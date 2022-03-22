375aa4e50b7ce959511833b3d69b69e015c55c1b
Kingswhale <hardeyreemyarfyc@gmail.com>
2022-03-22 05:10:35 +0100

    header

--- tinder-frontend/src/App.js
+++ tinder-frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './Header'
+import TinderCards from './TinderCards'
 
 function App() {
   //BEM class naming convention
   return (
     <div className='app'>
       <Header />
-      {/* tinder card */}
+      <TinderCards />
       {/* swip button */}
     </div>
   )
---
+++ tinder-frontend/src/TinderCards.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import './TinderCards.css'
+import TinderCard from 'react-tinder-card'
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: 'Elon Musk',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+    },
+    {
+      name: 'Afeez Aderemi',
+      url: 'https://avatars.githubusercontent.com/u/20205598?s=400&u=c2b73c55ba38239976583d55f3d7ce4df212bc2d&v=4',
+    },
+    {
+      name: 'Jack Dosey',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+    },
+  ])
+
+  const swiped = (direction, nameTODelete) => {
+    console.log('removing' + nameTODelete)
+  }
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen')
+  }
+  return (
+    <div className='tinderCards'>
+      <div className='ttinderCards_cardContainer'>
+        {people.map((person) => (
+          <TinderCard
+            className='swipe'
+            key={person.name}
+            preventSwipe={['up', 'down']}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className='card'
+            >
+              <h1>{person.name}</h1>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TinderCards
